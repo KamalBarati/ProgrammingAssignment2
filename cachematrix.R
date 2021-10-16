576c6bae151c283e79adeffff414ac6061f5db21
Kamal <barati.k21@gmail.com>
2021-10-16 18:15:12 +0330

    Update code description!

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,16 @@
 ## I have just simply rename the example in the
 ## Assignment description from "mean" and "m"
 ## to "solve" and "s"
+## We know that solve is a function in R to calculate inverse of a matrix
 
 ## The purpose here is Creating a matrix object, that can cache its inverse 
-## The part below creates a matrix that caches its inverse. Hereby x is a function argument; m is created in the makeCacheMatrix environment and set at NULL.
-# set is also created in the makeCacheMatrix environment and creates a function (y), wherein  both x and m are placed in the parent environment, here the input is set in x and m is set at NULL, reseting any value that may have been present for m in the global environment.
-# besides m and s, get, setinverse, getinverse and list are created in the makeCacheMatrix environment. 
+## The part below creates a matrix that caches its inverse. Hereby x is a function argument; s is created in the makeCacheMatrix environment and set at NULL.
+# set is also created in the makeCacheMatrix environment and creates a function (y), wherein  both x and s are placed in the parent environment, here the input is set in x and s is set at NULL, resetting any value that may have been present for s in the global environment.
+# besides x and s, get, setsolve, getsolve and list are created in the makeCacheMatrix environment. 
 # In get a function is created that retrieves x from the parent environment of makeCacheMatrix.  
-# In setinverse a function is created, whereby the inverse is set as m, and m is defined in the parent enviroment (meaning the inverse is set by retrieving m from the parent environment)
-# In getinverse m - being the inverse of x is retrieved. 
-# Finally in list all of the functions are asigned as an element and returned to the parent environment
+# In setsolve a function is created, whereby the inverse is set as s, and s is defined in the parent environment (meaning the inverse is set by retrieving s from the parent environment)
+# In getsolve s - being the inverse of x is retrieved. 
+# Finally in list all of the functions are assigned as an element and returned to the parent environment
 
 
 makeCacheMatrix <- function(x = matrix()) {
@@ -32,8 +33,8 @@ makeCacheMatrix <- function(x = matrix()) {
 ## The purpose here is Caching inverse
 # Cache inverse, by creating function cacheSolve in which a matrix is returned that is the inverse of x
 # cacheSolve starts with a function, with argument x and ellipses in which extra arguments can be placed.
-# Within the function m tries to retrieve the inverse of x by x$getinverse
-# if there is no result (aka m is NULL), cached data is retrieved for m and returned to the parent environment  
+# Within the function s tries to retrieve the inverse of x by x$getsolve
+# if there is no result (aka s is NULL), cached data is retrieved for s and returned to the parent environment  
 # next, the inverse of x is calculated and set
 
 
